5ecc72ccbae0b5da588ea1964a83a01c1c72bc00
Aman <133951515+amanXtech@users.noreply.github.com>
2024-02-29 17:07:57 +0530

    add some changes

--- deletion_in_linked_list.c
+++ deletion_in_linked_list.c
@@ -1,77 +1,108 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node
 {
     int data;
-    struct node* next;
-}node;
+    struct node *next;
+} node;
 
-
-void traversal(node* head) {
-    node* ptr = head;
-    while(ptr != NULL) {
-        printf("%d  " , ptr -> data);
-        ptr = ptr -> next;
+void traversal(node *head)
+{
+    node *ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d  ", ptr->data);
+        ptr = ptr->next;
     }
 }
 
-node* deletion_from_beg(node* head) {
-    node* ptr = head;
-    head = head -> next;
+node *deletion_from_beg(node *head)
+{
+    node *ptr = head;
+    head = head->next;
     free(ptr);
     return head;
 }
 
-node* deletion_from_end(node* head) {
-    node* p = head;
-    node* q = head -> next;
-    while(q -> next != NULL) {
-        p = p -> next;
-        q = q -> next;
+node *deletion_from_end(node *head)
+{
+    node *p = head;
+    node *q = head->next;
+    while (q->next != NULL)
+    {
+        p = p->next;
+        q = q->next;
     }
-    p -> next = NULL;
+    p->next = NULL;
     free(q);
     return head;
-
 }
 
-node* deletion_from_idx(node* head , int idx) {
-    node* p = head;
+node *deletion_from_idx(node *head, int idx)
+{
+    node *p = head;
     int count = 0;
-    while(count != idx -1) {
-        p = p -> next;
+    while (count != idx - 1)
+    {
+        p = p->next;
         count++;
     }
-    node* q = p -> next;
-    p -> next = p -> next -> next;
+    node *q = p->next;
+    p->next = q->next;
     free(q);
     return head;
+}
 
+node *delete_value(node *head, int val)
+{
+    node *p = head;
+    node *q = head->next;
+    if(head -> data == val) {
+        head = p -> next;
+        free(p);
+        return head;
+
+    }
+    while (q->data != val && q->next != NULL)
+    {
+        p = p->next;
+        q = q->next;
+    }
+    if (q->data == val)
+    {
+        p->next = q->next;
+        free(q);
+    } else {
+        printf("Element is not found\n");
+    }
+    return head;
 }
 
 int main()
 {
-    node* head = (node*) malloc(sizeof(node));
-    node* second = (node*) malloc(sizeof(node));
-    node* third = (node*) malloc(sizeof(node));
-    node* fourth = (node*) malloc(sizeof(node));
+    node *head = (node *)malloc(sizeof(node));
+    node *second = (node *)malloc(sizeof(node));
+    node *third = (node *)malloc(sizeof(node));
+    node *fourth = (node *)malloc(sizeof(node));
 
-    head -> data = 1;
-    head -> next = second;
-    second -> data = 2;
-    second -> next = third;
-    third -> data = 3;
-    third -> next = fourth;
-    fourth -> data = 4;
-    fourth -> next = NULL;
+    head->data = 1;
+    head->next = second;
+    second->data = 2;
+    second->next = third;
+    third->data = 3;
+    third->next = fourth;
+    fourth->data = 4;
+    fourth->next = NULL;
     traversal(head);
 
     printf("\nAfter deletion\n");
-    
+
     // head = deletion_from_end(head);
 
-    head = deletion_from_idx(head , 2);
+    // head = deletion_from_idx(head , 2);
+
+    head = delete_value(head, 1);
 
     traversal(head);
 
